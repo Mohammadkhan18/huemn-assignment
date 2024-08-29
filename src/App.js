7700bc0eb556215c499173f6ff45d4b233bf93f7
Mohammad Khan <95682232+Mohammadkhan18@users.noreply.github.com>
2024-08-29 23:12:38 +0530

    Add the first changes

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./css/main.css";
+import DisplayTodos from "./components/DisplayTodos";
+import Todos from "./components/Todos";
+
+import { motion } from "framer-motion";
+function App() {
+  return (
+    <div className="App">
+      <motion.h1
+        initial={{ y: -200 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 0.5 }}
+        whileHover={{ scale: 1.1 }}
+      >
+        Taskify
+      </motion.h1>
+      <motion.h4
+      initial={{ y: -400 }}
+      animate={{ y: 5 }}
+      transition={{ type: "spring", duration: 0.5 }}
+      whileHover={{ scale: 1.1 }}
+      >Simplify Your To-Do List</motion.h4>
+      <motion.div
+        initial={{ y: 1000 }}
+        animate={{ y: 0 }}
+        transition={{ type: "spring", duration: 1 }}
+      >
+        <Todos />
+        <DisplayTodos />
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
